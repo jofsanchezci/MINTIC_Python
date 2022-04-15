708f3ffb97b242c71322a33a5c9a9a561293a3a4
Joaquin Fernando Sanchez <js012108@gmail.com>
2022-04-15 11:42:57 -0500

    Create ejercicio_4.py

---
+++ ejercicio_4.py
@@ -0,0 +1,15 @@
+n1 = float(input("Introduce un número: ") )
+n2 = float(input("Introduce otro número: ") )
+opcion = 0
+           
+print("¿Qué quieres hacer? \n1) Sumar los dos números\n2) Restar los dos números\n3) Multiplicar los dos números")
+opcion = int(input("Introduce un número: ") )     
+
+if opcion == 1:
+    print("La suma de",n1,"+",n2,"es",n1+n2)
+elif opcion == 2:
+    print("La resta de",n1,"-",n2,"es",n1-n2)
+elif opcion == 3:
+    print("El producto de",n1,"*",n2,"es",n1*n2)
+else:
+    print("Opción incorrecta")
